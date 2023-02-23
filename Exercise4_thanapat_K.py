219d3003748b54e403217f800b25aecbdd506e48
az199x <126133960+az199x@users.noreply.github.com>
2023-02-23 10:57:40 +0700

    Create Exercise4_thanapat_K.py

---
+++ Exercise4_thanapat_K.py
@@ -0,0 +1,11 @@
+print ("score of thanapat K ")
+
+a = 60.23
+b = 72.67
+c = 62.33
+d = 80.53
+
+print ("Foundation English",":",a)
+print ("General Businessh",":",b)
+print ("Introduction to Computer Systems",":",c)
+print ("Computer Programming",":",d)
